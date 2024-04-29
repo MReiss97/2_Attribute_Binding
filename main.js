7ebcc84f211df3507d7698c932e55fcf54f40798
MReiss97 <maxreiss21@gmail.com>
2024-04-29 14:43:40 -0400

    added image and url

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
   data () {
     return {
-      product: 'Socks'
+      product: 'Socks',
+      image: './assets/images/socks_blue.jpg',
+      URL: 'https://www.vuemastery.com/courses/intro-to-vue-3/creating-the-vue-app-vue3'
     }
   }
 })
